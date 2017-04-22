05e223f4ec0e49ef81fc408ffdd666d1db3bd79a
Alexey Sharov <lehtenstaarn@gmail.com>
2017-04-22 19:04:56 +0300

    svistoperdelki

--- Sherlock/lib/components/game_field_component/board_cell_component/board_cell_component.dart
+++ Sherlock/lib/components/game_field_component/board_cell_component/board_cell_component.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
 import 'package:angular2/angular2.dart';
+import 'package:sherlock/components/game_field_component/board_cell_component/resolved_board_cell_component/resolved_board_cell_component.dart';
 
 @Component(
     selector: 'board-cell',
     styleUrls: const ['board_cell_component.css'],
     templateUrl: 'board_cell_component.html',
-    directives: const []
+    directives: const [ResolvedBoardCellComponent]
 )
 class BoardCellComponent {
-
+  String getResolvedClass() {
+    Random rand = new Random();
+    return "s${rand.nextInt(6)}${rand.nextInt(6)}";
+  }
 }
---
+++ Sherlock/lib/components/game_field_component/board_cell_component/resolved_board_cell_component/resolved_board_cell_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/angular2.dart';
+
+@Component(
+    selector: 'resolved-board-cell',
+    styleUrls: const ['resolved_board_cell_component.css'],
+    templateUrl: 'resolved_board_cell_component.html',
+    directives: const []
+)
+class ResolvedBoardCellComponent {
+
+}
