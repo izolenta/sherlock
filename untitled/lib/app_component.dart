d2c04f34fb0928f713089bf6771de3e03cdb5054
Alexey Sharov <lehtenstaarn@gmail.com>
2017-04-22 06:38:01 +0300

    very first

---
+++ untitled/lib/app_component.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2017, alea. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives],
+  providers: const [materialProviders],
+)
+class AppComponent {
+  // Nothing here yet. All logic is in HelloDialog.
+}
